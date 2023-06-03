244e339c692d2df8dfd69212d5d52fd743716b7b
Mayuresh Murudkar <mayureshmurudkar02@gmail.com>
2023-06-03 22:43:50 +0530

    Footer Section Complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 import "./Footer.scss";
+import { BsInstagram, BsFacebook, BsTwitter, BsLinkedin } from "react-icons/bs";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <section className="f-wrapper">
+        <div className="paddings innerWidth f-container">
+          <div className="flexCenter f-left">
+            <a href="" className="orangeText">
+              Residencies
+            </a>
+            <a href="/" className="orangeText">
+              Our Value
+            </a>
+            <a href="/" className="orangeText">
+              Contact Us
+            </a>
+            <a href="/" className="orangeText">
+              Get Started
+            </a>
+          </div>
+
+          <div className="f-center">
+            <img src="./logo.png" alt="Logo" width={180} />
+          </div>
+
+          <div className="flexColCenter f-right">
+            <div className="about-us">
+              <span className="orangeText">Follow Us</span>
+              <div className=" icons">
+                <BsInstagram />
+                <BsTwitter />
+                <BsLinkedin />
+                <BsFacebook />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flexCenter">
+          <span className="secondaryText">
+            &copy; 2023 Your Real Estate Company. All rights reserved.
+          </span>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "./GetStarted.scss";
 const GetStarted = () => {
-  return <div>GetStarted</div>;
+  return (
+    <div>
+      <div className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+          <div className="flexColCenter inner-container">
+            <span className="primaryText">Get Started With MM Real Estate</span>
+            <span className="secondaryText">
+              Explore More Properties with Us
+            </span>
+
+            <button className="button">
+              <a href="mailto:realestate@gmail.com">Get Started</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GetStarted;
